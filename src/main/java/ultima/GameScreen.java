73c10bdf10ecf9b32feec0618a0f60f2ddf604a8
pantinor <pantinor@gmail.com>
2015-09-24 20:14:00 -0400

    oops

--- src/main/java/ultima/GameScreen.java
+++ src/main/java/ultima/GameScreen.java
@@ -236,18 +236,18 @@ public class GameScreen extends BaseScreen {
             context.setParty(party);
             context.loadJournalEntries();
             
-            party.getMember(0).getPlayer().xp = 899;
-            party.getMember(0).getPlayer().hp = 999;
-            party.getMember(0).getPlayer().hpMax = 999;
-            party.getMember(0).getPlayer().intel = 99;
-            party.getMember(0).getPlayer().mp = 999;
-            sg.reagents = new int[]{90, 93, 94, 90, 90, 90, 90, 90};
-            for (Spell sp : Spell.values()) {
-                party.getSaveGame().mixtures[sp.ordinal()] = 99;
-            }
-            for (Virtue v : Virtue.values()) {
-                sg.karma[v.ordinal()] = 0;
-            }
+//            party.getMember(0).getPlayer().xp = 899;
+//            party.getMember(0).getPlayer().hp = 999;
+//            party.getMember(0).getPlayer().hpMax = 999;
+//            party.getMember(0).getPlayer().intel = 99;
+//            party.getMember(0).getPlayer().mp = 999;
+//            sg.reagents = new int[]{90, 93, 94, 90, 90, 90, 90, 90};
+//            for (Spell sp : Spell.values()) {
+//                party.getSaveGame().mixtures[sp.ordinal()] = 99;
+//            }
+//            for (Virtue v : Virtue.values()) {
+//                sg.karma[v.ordinal()] = 0;
+//            }
 //
 //            party.join(NpcDefaults.Geoffrey.name());
 //            party.join(NpcDefaults.Shamino.name());
@@ -261,7 +261,7 @@ public class GameScreen extends BaseScreen {
 //            sg.food = 30000;
 //            sg.gold = 999;
 //            sg.keys = 20;
-            sg.gems = 15;
+//            sg.gems = 15;
 //            sg.moves = 2800;
 //            sg.stones = 0xff;
 //            sg.runes = 0xff;
@@ -285,8 +285,8 @@ public class GameScreen extends BaseScreen {
             //sg.items |= Constants.Item.BOOK.getLoc();
             
             //load the surface world first
-            //loadNextMap(Maps.WORLD, sg.x, sg.y);
-            loadNextMap(Maps.WORLD, 141, 90);
+            loadNextMap(Maps.WORLD, sg.x, sg.y);
+            //loadNextMap(Maps.WORLD, 141, 90);
 
             //load the dungeon if save game starts in dungeon
             if (Maps.get(sg.location) != Maps.WORLD) {
